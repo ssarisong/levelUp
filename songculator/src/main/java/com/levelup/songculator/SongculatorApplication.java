6a0ae2e428189c81ea79ee8c459c57585e5f09ee
ssarisong <thdtnwls2989@naver.com>
2025-06-16 15:20:06 +0900

    init: generate songculator

---
+++ songculator/src/main/java/com/levelup/songculator/SongculatorApplication.java
@@ -0,0 +1,13 @@
+package com.levelup.songculator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SongculatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SongculatorApplication.class, args);
+	}
+
+}
---
+++ songculator/src/test/java/com/levelup/songculator/SongculatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.levelup.songculator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SongculatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
